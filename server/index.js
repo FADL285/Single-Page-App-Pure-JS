ca06c19308d00a0e9ad84e6e817f61adfa49e421
Mohamed Fadl <mohamed.fadl2852@gmail.com>
2022-01-05 01:07:14 +0200

    Added Middleware to solve MEMI type error

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const path = require('path');
 
 const app = express();
 
+app.use('/assets', express.static(path.resolve('dist', 'assets')));
+
 app.get('/*', (req, res) => {
   res.sendFile(path.resolve('dist', 'index.html'));
 });
